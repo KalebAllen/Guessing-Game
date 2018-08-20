f915089315bf454679fc5e4221bba67cd85959e7
Kaleb Allen <kaleballen@interapts-macbook-pro.local>
2018-08-20 07:54:14 -0500

    Initial Commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Kaleb Allen on 8/20/18.
+//  Copyright © 2018 Kaleb Allen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
